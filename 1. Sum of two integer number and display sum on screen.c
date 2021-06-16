9b4f86f6d76f77f03ebb35c269940f6a5142a3a2
ARPIT GARG <77561392+grarpit621@users.noreply.github.com>
2021-06-16 12:57:51 +0530

    Add files via upload

---
+++ 1. Sum of two integer number and display sum on screen.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main()
+{
+    int a,b,s;
+    printf("enter a value:");
+    scanf("%d",&a);
+    printf("enter another value:");
+    scanf("%d",&b);
+    s=(a+b);
+    printf("%d",s);
+    return 0;
+}
