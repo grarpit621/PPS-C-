e6cfd99ddb0974aec3391510e81ffb9309d01e70
ARPIT GARG <77561392+grarpit621@users.noreply.github.com>
2021-11-19 11:16:17 +0530

    Update 17.Write delete duplicate elements in an array..c

--- 17.Write delete duplicate elements in an array..c
+++ 17.Write delete duplicate elements in an array..c
@@ -1,34 +1,51 @@
-#include <stdio.h>
-int main()
-{
-  int n, a[100], b[100], count = 0, c, d;
-
-  printf("Enter number of elements in array\n");
-  scanf("%d", &n);
-
-  printf("Enter %d integers\n", n);
-
-  for (c = 0; c < n; c++)
-    scanf("%d", &a[c]);
-
-  for (c = 0; c < n; c++)
-  {
-    for (d = 0; d < count; d++)
-    {
-      if(a[c] == b[d])
-        break;
-    }
-    if (d == count)
-    {
-      b[count] = a[c];
-      count++;
-    }
-  }
-
-  printf("Array obtained after removing duplicate elements:\n");
-
-  for (c = 0; c < count; c++)
-    printf("%d\n", b[c]);
-
-  return 0;
-}
+#include <stdio.h>  
+#include <conio.h>  
+int main ()  
+{  
+    // declare local variables   
+    int arr[20], i, j, k, size;  
+      
+    printf (" Define the number of elements in an array: ");  
+    scanf (" %d", &size);  
+      
+    printf (" \n Enter %d elements of an array: \n ", size);  
+    // use for loop to enter the elements one by one in an array  
+    for ( i = 0; i < size; i++)  
+    {  
+        scanf (" %d", &arr[i]);  
+    }  
+      
+      
+    // use nested for loop to find the duplicate elements in array  
+    for ( i = 0; i < size; i ++)  
+    {  
+        for ( j = i + 1; j < size; j++)  
+        {  
+            // use if statement to check duplicate element  
+            if ( arr[i] == arr[j])  
+            {  
+                // delete the current position of the duplicate element  
+                for ( k = j; k < size - 1; k++)  
+                {  
+                    arr[k] = arr [k + 1];  
+                }  
+                // decrease the size of array after removing duplicate element  
+                size--;  
+                  
+            // if the position of the elements is changes, don't increase the index j  
+                j--;      
+            }  
+        }  
+    }  
+      
+      
+    /* display an array after deletion or removing of the duplicate elements */  
+    printf (" \n Array elements after deletion of the duplicate elements: ");  
+      
+    // for loop to print the array  
+    for ( i = 0; i < size; i++)  
+    {  
+        printf (" %d \t", arr[i]);  
+    }  
+    return 0;  
+}  
